4732fda403b9a4864f7236df1729011c350794eb
maarcoscuesta18 <marcosyjaime2@gmail.com>
2022-11-09 19:56:44 +0100

    Initial commit

---
+++ app/src/main/java/com/marcoscuesta/uoc/MainActivity.java
@@ -0,0 +1,53 @@
+package com.marcoscuesta.uoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JsResult;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        // Hide the title bar.
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.loadUrl("https://cv.uoc.edu/auth?campus-nplincampus");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+
+        } else {
+            super.onBackPressed();
+            webView.loadUrl("https://cv.uoc.edu/auth?campus-nplincampus");
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.destroy();
+    }
+}
