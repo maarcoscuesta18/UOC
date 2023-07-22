b0c480346cfc3f19e2e6c6e1c65f347b3eb52f8f
maarcoscuesta18 <marcosyjaime2@gmail.com>
2023-07-22 20:08:06 +0200

    ajuste goback

--- app/src/main/java/com/marcoscuesta/uoc/MainActivity.java
+++ app/src/main/java/com/marcoscuesta/uoc/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.JsResult;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.getSettings().setAllowFileAccess(true);
         webView.loadUrl("https://cv.uoc.edu/auth?campus-nplincampus");
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -49,5 +51,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         webView.destroy();
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 }
